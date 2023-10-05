20be34611d2eef907bc1d492fdfd4d90ced0ab50
Oindrila-Chakraborty <ochakraborty937@gmail.com>
2023-10-05 21:44:27 +0530

    fixed image path for icons

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -6,52 +6,52 @@ export default [
     },
     {
         id:2,
-        icon:'src/assets/images/icon2.svg',
+        icon:'icon2.svg',
         label:'Orders'
     },
     {
         id:3,
-        icon:'src/assets/images/icon3.svg',
+        icon:'icon3.svg',
         label:'Products'
     },
     {
         id:4,
-        icon:'src/assets/images/icon4.svg',
+        icon:'icon4.svg',
         label:'Delivery'
     },
     {
         id:5,
-        icon:'src/assets/images/icon5.svg',
+        icon:'icon5.svg',
         label:'Marketing'
     },
     {
         id:6,
-        icon:'src/assets/images/icon6.svg',
+        icon:'icon6.svg',
         label:'Analytics'
     },
     {
         id:7,
-        icon:'src/assets/images/icon7.svg',
+        icon:'icon7.svg',
         label:'Payouts'
     },
     {
         id:8,
-        icon:'src/assets/images/icon8.svg',
+        icon:'icon8.svg',
         label:'Discount'
     },
     {
         id:9,
-        icon:'src/assets/images/icon9.svg',
+        icon:'icon9.svg',
         label:'Audience'
     },
     {
         id:10,
-        icon:'src/assets/images/icon10.png',
+        icon:'icon10.png',
         label:'Appearance'
     },
     {
         id:11,
-        icon:'src/assets/images/icon11.png',
+        icon:'icon11.png',
         label:'Plugins'
     },
 ]
